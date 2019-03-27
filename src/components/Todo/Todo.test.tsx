b797290a5144b78272028d7f7583b79c5cf036a1
Quy Tran <quytran288@gmail.com>
2019-03-26 20:34:18 -0700

    Implementing Unit Test for Todo rendering

--- src/components/Todo/Todo.test.tsx
+++ src/components/Todo/Todo.test.tsx
@@ -9,7 +9,9 @@ describe('<Todo /> rendering', () => {
 
 	beforeEach(() => {
 		const title = "Default title";
+		const isHovering = false;
 		wrapper = enzyme.shallow(<Todo title={title} />);
+		wrapper.setState({isHovering});
 		wrapperInstance = wrapper.instance();
 	});
 
@@ -17,43 +19,59 @@ describe('<Todo /> rendering', () => {
 		expect(wrapper.find(Checkbox).length).toEqual(1);
 	});
 
-	it('should render title', () => {
+	it('should render title correctly', () => {
 		const title = "Bring clothes to Goodwill";
-		wrapper.setProps(title);
+		wrapper.setProps({title});
 		expect(wrapper.find('.todoTitle').text()).toEqual(title);
 	});
 
-	it('should render schedule when schedule is set', () => {
-		const schedule = "03/25/2019 22:42";
-		wrapper.setProps(schedule);
+	it('shouldn\'t render Drag icon, Action icon when not hover', () => {
+		const isHovering = false;
+		wrapper.setState({isHovering});
+		expect(wrapper.find('.todoDragIcon').length).toEqual(0);
+		expect(wrapper.find('.todoActionIcon').length).toEqual(0);
+	});
+
+	it('should render Drag icon, Action icon when hover', () => {
+		const isHovering = true;
+		wrapper.setState({isHovering});
+		expect(wrapper.find('.todoDragIcon').length).toEqual(1);
+		expect(wrapper.find('.todoActionIcon').length).toEqual(1);
+	});
+
+	it('should render Schedule area when schedule is set', () => {
+		const schedule = '03/25/2019 22:42';
+		wrapper.setProps({schedule});
 		expect(wrapper.find('.todoSchedule').text()).toEqual(schedule);
 	});
 
-	it('shouldn\'t display Drag icon, Action icon when not hover', () => {
-
+	it('shouldn\'t render Schedule area when schedule is not set and not hover', () => {
+		const schedule = null;
+		const isHovering = false;
+		wrapper.setProps({schedule});
+		wrapper.setState({isHovering});
+		expect(wrapper.find('.todoSchedule').length).toEqual(0);
 	});
 
-	it('should display Drag icon, Action icon when hover', () => {
-
-	});
-
-	it('shouldn\'t display Calendar icon when not hover and schedule is not set', () => {
-
-	});
-
-	it('should display Calendar icon when hover and schedule is not set', () => {
-
+	it('should display Schedule area when schedule is not set and is hovering', () => {
+		const schedule = null;
+		const isHovering = true;
+		wrapper.setProps({schedule});
+		wrapper.setState({isHovering});
+		expect(wrapper.find('.todoSchedule').length).toEqual(1);
 	});
 
 	it('isHover state should be false at beginning', () => {
 		expect(wrapperInstance.state.isHover).toEqual(false);
 	});
 
-	it('should change isHover to true when mouse entered', () => {
-		// expect(wrapperInstance.state.isHover).toEqual(false);
-	});
-
-	it('should change isHover to false when mouse leaved', () => {
-		// expect(wrapperInstance.state.isHover).toEqual(false);
-	});
+	// it('should change isHover to true when mouse entered', () => {
+	// 	// Simulate
+	// 	expect(wrapperInstance.state.isHover).toEqual(true);
+	// });
+    //
+	// it('should change isHover to false when mouse leaved', () => {
+	// 	// Simulate
+	// 	expect(wrapperInstance.state.isHover).toEqual(true);
+	// });
 });
