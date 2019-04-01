37aa01dc55d0a6c6448d72c64e4a0ac70f8917ba
Quy Tran <quytran288@gmail.com>
2019-03-31 22:24:32 -0700

    Edit Icon unit test

--- src/components/UI/Icon/Icon.test.tsx
+++ src/components/UI/Icon/Icon.test.tsx
@@ -1,12 +1,17 @@
 import * as enzyme from 'enzyme';
 import * as React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons';
 
 import {IconType, IconVersion} from '../../../constants';
 import Icon from './Icon';
 
+// Need to explicitly add icon here because test files is not under root component (App)
+library.add(faCommentDots);
+
 describe('<Icons > render', () => {
-   it('should render icon correctly', () => {
-      const wrapper = enzyme.shallow(<Icon type={IconType.Comment} version={IconVersion.Far} />);
-      expect(wrapper.find('.icon-' + IconVersion.Far + '-' + IconType.Comment).length).toEqual(1);
+   it('should render a font awesome icon', () => {
+      const wrapper = enzyme.mount(<Icon type={IconType.Comment} version={IconVersion.Far} />);
+      expect(wrapper.find('.fa-' + IconType.Comment).length).toEqual(1);
    });
 });
